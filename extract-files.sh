2b87c32ec11ec36e4a1baa089b8106f85486bf74
SamarV-121 <samarvispute121@pm.me>
2021-09-03 14:45:57 +0200

    [FIXUP] rosemary: add ims blobs
    - F linker  : CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi" referenced by "/system/lib/libsink.so"
    
    Fixes: 500ef1db0469f9e3521d68c6550973cd8c6db9f4
    Change-Id: I97402a7dcae98ec97168dec906bcad20df85c02c
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,14 @@ if [ -z "${SRC}" ]; then
     SRC=adb
 fi
 
+function blob_fixup {
+    case "${1}" in
+        lib/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
