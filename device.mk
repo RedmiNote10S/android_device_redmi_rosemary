100a0d8e4f306ba932051c848759e584e852cf9d
Akshat <79436731+akshat942@users.noreply.github.com>
2021-12-06 02:27:13 -0800

    rosemary: add bootctrl

--- device.mk
+++ device.mk
@@ -115,37 +115,37 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # A/B
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl-1.1-mtkimpl \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.0
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
-PRODUCT_PACKAGES += \
-  update_engine \
-  update_engine_sideload \
-  update_verifier
-
-PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
-
+    
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-recovery \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -169,10 +169,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
@@ -186,8 +182,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+    
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
