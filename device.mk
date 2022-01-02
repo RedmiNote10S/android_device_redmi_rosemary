a49396425761618e7519621817df801c772d87b9
Myst33d <notmyst33d@gmail.com>
2022-01-02 23:37:52 +0600

    rosemary: Delete more stuff

--- device.mk
+++ device.mk
@@ -43,21 +43,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
-
-# RRO (Runtime Resource Overlay)
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(DEVICE_PATH)/overlay-lineage/packages/apps/Snap
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(DEVICE_PATH)/overlay/packages/apps/CarrierConfig
-
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-mtkimpl
     android.hardware.boot@1.1-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
@@ -67,33 +58,13 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    factory_init.connectivity.rc \
-    fstab.emmc \
-    init.aee.rc \
-    init.modem.rc \
-    init.project.rc \
-    meta_init.connectivity.rc \
-    meta_init.rc \
-    factory_init.project.rc \
-    fstab.enableswap \
-    init.ago.rc \
-    init.mt6785.rc \
-    init.sensor_1_0.rc \
-    meta_init.modem.rc \
-    multi_init.rc \
-    factory_init.rc \
-    fstab.mt6785 \
-    init.connectivity.rc \
-    init.mt6785.usb.rc \
-    init.stnfc.rc \
-    meta_init.project.rc \
-    ueventd.mtk.rc
 
 # Ramdisk For Kernel
 PRODUCT_COPY_FILES += \
@@ -104,10 +75,6 @@ PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
-
-# Recovery
-PRODUCT_PACKAGES += \
-    init.recovery.mt6785.rc
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
