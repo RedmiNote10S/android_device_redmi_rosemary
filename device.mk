c3df5b1340948fcfd4972b29624e7dc7df9dbee6
Myst33d <notmyst33d@gmail.com>
2022-02-21 21:35:49 +0600

    rosemary: Initial changes for OSS vendor

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/rosemary
+DEVICE_PATH := device/redmi/rosemary
 
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
@@ -58,9 +58,47 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_BUILD_SUPER_PARTITION := false
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
+# Mtkperf
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
+# Common NFC
+PRODUCT_PACKAGES += \
+    SecureElement \
+    Tag \
+    com.android.nfc_extras
+
+# NfcNci
+PRODUCT_PACKAGES += \
+    NfcNci
+
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.mediatek
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
+
+# Overlays
+PRODUCT_PACKAGES += \
+    RosemaryFrameworks \
+    RosemaryTelephony
+
+# Props
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/props/build_rosemary.prop:$(TARGET_COPY_OUT_SYSTEM)/build_rosemary.prop
+
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -70,4 +108,4 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_TARGET_VNDK_VERSION := 30
 
 # Inherit vendor
-$(call inherit-product, vendor/xiaomi/rosemary/rosemary-vendor.mk)
+$(call inherit-product, vendor/redmi/rosemary/rosemary-vendor.mk)
