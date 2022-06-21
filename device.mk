b3be6151fed36a6c2f3b44d6c52f809fc3910e04
Woomymy <github@woomy.ovh>
2022-06-21 17:52:55 +0200

    bootctrl: Update to version 1.2
    * Implement getActiveBootSlot
    
    Signed-Off-By: Woomy <github@woomy.ovh>

--- device.mk
+++ device.mk
@@ -111,9 +111,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-mtkimpl.recovery
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
