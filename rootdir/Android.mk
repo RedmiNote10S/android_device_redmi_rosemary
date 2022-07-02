f18fd2731f8d413238c0d6c4f4218d0e29b3b824
Beru Hinode <windowz414@1337.lgbt>
2022-07-02 09:21:13 +0000

    rosemary/rootdir: Build modified init scripts into vendor_overlay
    * Our vendor is prebuilt so what we build into there clashes with
      the prebuilt. So let's overlay them instead.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,7 +38,7 @@ LOCAL_MODULE       := init.mt6785.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mt6785.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -62,7 +62,7 @@ LOCAL_MODULE       := init.rosemary.incall_fixup.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.rosemary.incall_fixup.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -70,7 +70,7 @@ LOCAL_MODULE       := init.rosemary.incall_fixup.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.rosemary.incall_fixup.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
