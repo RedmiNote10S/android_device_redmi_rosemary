df699a02c91373303e91a82661e90f587eb9ecd9
Beru Hinode <windowz414@1337.lgbt>
2022-07-21 15:25:32 +0000

    Revert "rosemary/rootdir: Integrate Woomy's In-call Volume Fix"
    This reverts commit 4bdeab6f104afb783b1df48b5ff667fc6753612b.

--- device.mk
+++ device.mk
@@ -179,8 +179,6 @@ PRODUCT_PACKAGES += \
     init.mt6785.rc \
     init.sensor_1_0.rc \
     fstab.mt6785 \
-    init.rosemary.incall_fixup.rc \
-    init.rosemary.incall_fixup.sh
 
 # Ramdisk For Kernel
 PRODUCT_COPY_FILES += \
@@ -206,6 +204,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/xyz.extras.xml \
     $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/xyz.extras.xml 
 
-# TinyMix - Needed for in-call volume
-PRODUCT_PACKAGES += \
-    tinymix
