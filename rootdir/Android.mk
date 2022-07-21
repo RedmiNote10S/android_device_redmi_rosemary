3e4b06ac1ffb7721d2ef94f69ff176c82fc1a76e
Beru Hinode <windowz414@1337.lgbt>
2022-07-21 15:25:32 +0000

    Revert "rosemary/rootdir: Integrate Woomy's In-call Volume Fix"
    This reverts commit d5f946916f6f899f4d2223c74da7afe0e0515eaf.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -58,22 +58,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rosemary.incall_fixup.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rosemary.incall_fixup.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rosemary.incall_fixup.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.rosemary.incall_fixup.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.sensor_1_0.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
