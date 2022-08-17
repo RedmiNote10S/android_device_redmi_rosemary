fc5720ac45c6562426c87d89fe569370671c19bc
Woomymy <github@woomy.ovh>
2022-08-17 13:55:14 +0200

    rosemary/makefiles: Prepare for Lineage
    Signed-off-by: Woomymy <github@woomy.ovh>

--- device.mk
+++ device.mk
@@ -289,7 +289,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-kasumi \
     $(DEVICE_PATH)/overlay-lineage
 
 # Sensors
