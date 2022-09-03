55becbebc311c06b53c52a4f3c14e15ab7975381
Woomy <woomy@woomy.be>
2022-09-04 00:11:15 +0200

    rosemart/init: Override default st nfc init rc
    * Makr sure st nfc hao is stopped by default and started only on
      rosemary
    
    * Prevents logs shitspam on secret/maltose
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := android.hardware.nfc@1.2-service.st.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/android.hardware.nfc@1.2-service.st.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mediatek.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
