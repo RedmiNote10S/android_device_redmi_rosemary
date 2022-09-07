1362d7dcf5a4c42a1f8f8b7c43de9945edd9db45
Beru Hinode <windowz414@1337.lgbt>
2022-09-07 07:57:35 +0000

    rosemary/extract-files: Remove duplicate `blob_fixup`
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>
    Change-Id: Iad6059a0ad3fd0e35d7bc6e22d23d9af3bfcedf1

--- extract-files.sh
+++ extract-files.sh
@@ -64,14 +64,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        lib/libsink.so)
-            "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
-            ;;
-    esac
-}
-
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
