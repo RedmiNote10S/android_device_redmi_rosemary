b2ae131f3cf775ca670dd1d8a4c87c3714fba385
Beru Hinode <windowz414@1337.lgbt>
2022-09-07 20:17:07 +0000

    Revert "[DO NOT MERGE ANYWHERE] rosemary/rootdir: Logcat to cache"
    * We got ADB now, this is unnecessary.
    
    This reverts commit 04f8e8bd165aa185a29660928989bd957bb504d2.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -169,11 +169,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.logger.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.logger.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
