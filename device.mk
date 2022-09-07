99538f01a008ffe957bd6c83c17c3f725de3c20f
Beru Hinode <windowz414@1337.lgbt>
2022-09-07 20:08:46 +0000

    rosemary/device: Build vendor variant of camera interface v2.6
    * Camera HAL service wouldn't start without this.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>
    Change-Id: I813e0c16337c8067ed6bec1ff1ca5cb66571a783

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.5.vendor
 
 # Charger
