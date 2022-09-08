RedmiNote10S/android_device_redmi_rosemary

commit a4f9f634320cde1bc44ed513430ce80ec2bec80f

rosemary: Kang AIDL vibrator service from Ronin
* AIDL vibrator service from Xiaomi MediaTek devices doesn't properly
  work on AOSP.

Signed-off-by: Beru Hinode <windowz414@1337.lgbt>
Change-Id: I6ba798f1a2717a7b75abee65a677e874ceb11e3b

device.mk
@@ -500,10 +500,6 @@ PRODUCT_PACKAGES += \
     vndservice_manager \
     vndservice_manager.vendor
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.rosemary
-
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge

vibrator/Android.bp
@@ -1,32 +1,0 @@
-cc_library_static {
-    name: "libvibratorrosemary",
-    vendor: true,
-    shared_libs: [
-        "libbase",
-        "libbinder_ndk",
-        "android.hardware.vibrator-V1-ndk_platform",
-    ],
-    export_include_dirs: ["include"],
-    srcs: ["Vibrator.cpp"],
-    visibility: [
-        ":__subpackages__",
-        "//hardware/interfaces/tests/extension/vibrator:__subpackages__",
-    ],
-}
-
-cc_binary {
-    name: "android.hardware.vibrator-service.rosemary",
-    relative_install_path: "hw",
-    vendor: true,
-    init_rc: ["vibrator-rosemary.rc"],
-    vintf_fragments: ["vibrator-rosemary.xml"],
-    shared_libs: [
-        "libbase",
-        "libbinder_ndk",
-        "android.hardware.vibrator-V1-ndk_platform",
-    ],
-    static_libs: [
-        "libvibratorrosemary",
-    ],
-    srcs: ["main.cpp"],
-}

vibrator/Vibrator.cpp
@@ -1,176 +1,0 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "vibrator-impl/Vibrator.h"
-
-#define LOG_TAG "android.hardware.vibrator-service.rosemary"
-
-#include <android-base/logging.h>
-#include <thread>
-#include <fstream>
-
-#define PCONCAT(A, B) A/B
-#define STRINGIFY_INNER(s) #s
-#define STRINGIFY(s) STRINGIFY_INNER(s)
-#define VIBRA_NODE(n) STRINGIFY(PCONCAT(/sys/devices/platform/aw8622, n))
-
-namespace aidl {
-namespace android {
-namespace hardware {
-namespace vibrator {
-
-template <typename T>
-static void write_sysfs_node(const std::string& path, const T& val) {
-    std::ofstream node(path);
-    node << val;
-}
-
-ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
-    LOG(INFO) << "Vibrator reporting capabilities";
-    *_aidl_return = IVibrator::CAP_PERFORM_CALLBACK;
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus Vibrator::off() {
-    LOG(INFO) << "Vibrator off";
-
-    write_sysfs_node(VIBRA_NODE(index), 0);
-    write_sysfs_node(VIBRA_NODE(activate), 0);
-
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus Vibrator::on(int32_t timeoutMs,
-                                __attribute__((unused)) const std::shared_ptr<IVibratorCallback>& callback) {
-    LOG(INFO) << "Vibrator on for timeoutMs: " << timeoutMs;
-    LOG(INFO) << VIBRA_NODE(duration);
-    write_sysfs_node(VIBRA_NODE(duration), timeoutMs);
-    write_sysfs_node(VIBRA_NODE(activate), 1);
-
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus Vibrator::perform(Effect effect, __attribute__((unused)) EffectStrength strength,
-                                     __attribute__((unused)) const std::shared_ptr<IVibratorCallback>& callback,
-                                     int32_t* _aidl_return) {
-
-    uint32_t index = 0;
-    uint32_t timeoutMs = 0;
-
-    switch (effect) {
-        case Effect::TICK:
-            index = WAVEFORM_TICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_TICK_EFFECT_MS;
-            break;
-        case Effect::TEXTURE_TICK:
-            index = WAVEFORM_TEXTURE_TICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_TEXTURE_TICK_EFFECT_MS;
-            break;
-        case Effect::CLICK:
-            index = WAVEFORM_CLICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_CLICK_EFFECT_MS;
-            break;
-        case Effect::HEAVY_CLICK:
-            index = WAVEFORM_HEAVY_CLICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_HEAVY_CLICK_EFFECT_MS;
-            break;
-        case Effect::DOUBLE_CLICK:
-            index = WAVEFORM_DOUBLE_CLICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_DOUBLE_CLICK_EFFECT_MS;
-            break;
-        case Effect::THUD:
-            index = WAVEFORM_THUD_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_THUD_EFFECT_MS;
-            break;
-        case Effect::POP:
-            index = WAVEFORM_TICK_EFFECT_INDEX;
-            timeoutMs = WAVEFORM_POP_EFFECT_MS;
-            break;
-        default:
-            return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-    }
-
-    write_sysfs_node(VIBRA_NODE(index), index);
-    ndk::ScopedAStatus ret = on(timeoutMs, nullptr);
-        
-    if (!ret.isOk()) {
-        return ret;
-    } else {
-        *_aidl_return = timeoutMs;
-    }
-
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_return) {
-    *_aidl_return = {
-        Effect::CLICK,
-        Effect::TICK,
-        Effect::TEXTURE_TICK,
-        Effect::HEAVY_CLICK,
-        Effect::DOUBLE_CLICK,
-        Effect::THUD,
-        Effect::POP
-    };
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus Vibrator::setAmplitude(__attribute__((unused)) float amplitude) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::setExternalControl(__attribute__((unused)) bool enabled) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getCompositionDelayMax(__attribute__((unused)) int32_t* maxDelayMs) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getCompositionSizeMax(__attribute__((unused)) int32_t* maxSize) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedPrimitives(__attribute__((unused)) std::vector<CompositePrimitive>* supported) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getPrimitiveDuration(__attribute__((unused)) CompositePrimitive primitive,
-                                                  __attribute__((unused)) int32_t* durationMs) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::compose(__attribute__((unused)) const std::vector<CompositeEffect>& composite,
-                                     __attribute__((unused)) const std::shared_ptr<IVibratorCallback>& callback) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedAlwaysOnEffects(__attribute__((unused)) std::vector<Effect>* _aidl_return) {
-    return getSupportedEffects(_aidl_return);
-}
-
-ndk::ScopedAStatus Vibrator::alwaysOnEnable(__attribute__((unused)) int32_t id, __attribute__((unused)) Effect effect, __attribute__((unused)) EffectStrength strength) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::alwaysOnDisable(__attribute__((unused)) int32_t id) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-}  // namespace vibrator
-}  // namespace hardware
-}  // namespace android
-}  // namespace aidl

vibrator/include/vibrator-impl/Vibrator.h
@@ -1,74 +1,0 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#pragma once
-
-#include <aidl/android/hardware/vibrator/BnVibrator.h>
-
-
-
-// Values inspired from cannon(g) vibrator HAL
-// https://github.com/StatiXOS/android_device_xiaomi_cannon/commit/f95eff04bbaa6bbbbd340a200ce029ffb889738e
-
-// Define durations for waveforms
-static constexpr uint32_t WAVEFORM_TICK_EFFECT_MS = 10;
-static constexpr uint32_t WAVEFORM_TEXTURE_TICK_EFFECT_MS = 20;
-static constexpr uint32_t WAVEFORM_CLICK_EFFECT_MS = 15;
-static constexpr uint32_t WAVEFORM_HEAVY_CLICK_EFFECT_MS = 30;
-static constexpr uint32_t WAVEFORM_DOUBLE_CLICK_EFFECT_MS = 60;
-static constexpr uint32_t WAVEFORM_THUD_EFFECT_MS = 35;
-static constexpr uint32_t WAVEFORM_POP_EFFECT_MS = 15;
-
-// Select waveform index from firmware through index list
-static constexpr uint32_t WAVEFORM_TICK_EFFECT_INDEX = 1;
-static constexpr uint32_t WAVEFORM_TEXTURE_TICK_EFFECT_INDEX = 4;
-static constexpr uint32_t WAVEFORM_CLICK_EFFECT_INDEX = 2;
-static constexpr uint32_t WAVEFORM_HEAVY_CLICK_EFFECT_INDEX = 5;
-static constexpr uint32_t WAVEFORM_DOUBLE_CLICK_EFFECT_INDEX = 6;
-static constexpr uint32_t WAVEFORM_THUD_EFFECT_INDEX = 7;
-
-namespace aidl {
-namespace android {
-namespace hardware {
-namespace vibrator {
-
-class Vibrator : public BnVibrator {
-    ndk::ScopedAStatus getCapabilities(int32_t* _aidl_return) override;
-    ndk::ScopedAStatus off() override;
-    ndk::ScopedAStatus on(int32_t timeoutMs,
-                          const std::shared_ptr<IVibratorCallback>& callback) override;
-    ndk::ScopedAStatus perform(Effect effect, EffectStrength strength,
-                               const std::shared_ptr<IVibratorCallback>& callback,
-                               int32_t* _aidl_return) override;
-    ndk::ScopedAStatus getSupportedEffects(std::vector<Effect>* _aidl_return) override;
-    ndk::ScopedAStatus setAmplitude(float amplitude) override;
-    ndk::ScopedAStatus setExternalControl(bool enabled) override;
-    ndk::ScopedAStatus getCompositionDelayMax(int32_t* maxDelayMs);
-    ndk::ScopedAStatus getCompositionSizeMax(int32_t* maxSize);
-    ndk::ScopedAStatus getSupportedPrimitives(std::vector<CompositePrimitive>* supported) override;
-    ndk::ScopedAStatus getPrimitiveDuration(CompositePrimitive primitive,
-                                            int32_t* durationMs) override;
-    ndk::ScopedAStatus compose(const std::vector<CompositeEffect>& composite,
-                               const std::shared_ptr<IVibratorCallback>& callback) override;
-    ndk::ScopedAStatus getSupportedAlwaysOnEffects(std::vector<Effect>* _aidl_return) override;
-    ndk::ScopedAStatus alwaysOnEnable(int32_t id, Effect effect, EffectStrength strength) override;
-    ndk::ScopedAStatus alwaysOnDisable(int32_t id) override;
-};
-
-}  // namespace vibrator
-}  // namespace hardware
-}  // namespace android
-}  // namespace aidl

vibrator/main.cpp
@@ -1,35 +1,0 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "vibrator-impl/Vibrator.h"
-
-#include <android-base/logging.h>
-#include <android/binder_manager.h>
-#include <android/binder_process.h>
-
-using aidl::android::hardware::vibrator::Vibrator;
-
-int main() {
-    ABinderProcess_setThreadPoolMaxThreadCount(0);
-    std::shared_ptr<Vibrator> vib = ndk::SharedRefBase::make<Vibrator>();
-
-    const std::string instance = std::string() + Vibrator::descriptor + "/default";
-    binder_status_t status = AServiceManager_addService(vib->asBinder().get(), instance.c_str());
-    CHECK(status == STATUS_OK);
-
-    ABinderProcess_joinThreadPool();
-    return EXIT_FAILURE;  // should not reach
-}

vibrator/vibrator-rosemary.rc
@@ -1,4 +1,0 @@
-service vibrator-rosemary /vendor/bin/hw/android.hardware.vibrator-service.rosemary
-    class hal
-    user system
-    group system

vibrator/vibrator-rosemary.xml
@@ -1,7 +1,0 @@
-<manifest version="1.0" type="device">
-    <hal format="aidl">
-        <name>android.hardware.vibrator</name>
-        <fqname>IVibrator/default</fqname>
-    </hal>
-</manifest>
-

commit d5ade50e6e7519cd6c1917f3bd7bc8ababed1ace

rosemary/device: Build AOSP vibrator NDK platform v1
* The commit where we kanged vibrator HAL from Ronin would also
  remove this platform, since it was served as a dependency to our
  old vibrator HAL. Let's build it discretely as it would fix the
  dependency breakage and so caused boot failures.

Fixes: a4f9f634320cde1bc44ed513430ce80ec2bec80f
Signed-off-by: Beru Hinode <windowz414@1337.lgbt>
Change-Id: I633a20ed277429e5a320a2a6d02558ffb9626136

device.mk
@@ -495,6 +495,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.0.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-V1-ndk_platform
+
 # VNDService
 PRODUCT_PACKAGES += \
     vndservice_manager \