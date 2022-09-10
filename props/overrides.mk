5b20f55e19dc78fb0d8407968e21cb88f7c34388
Beru Hinode <windowz414@1337.lgbt>
2022-09-10 16:05:03 +0000

    rosemary/props/overrides: Force ban SF from latching unsignaled
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>
    Change-Id: I22d4dd9f84de6cf0e8168a25bda84cef49f8f0c0

--- props/overrides.mk
+++ props/overrides.mk
@@ -9,3 +9,7 @@
 # System props
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     ro.control_privapp_permissions=log
+
+# Vendor props
+PRODUCT_VENDOR_PROPERTY_OVERRIDES += \
+    vendor.debug.sf.latch_unsignaled=0
