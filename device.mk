9fab4d238c512e389250dcfb6f1689760023e2ca
Woomymy <github@woomy.ovh>
2022-09-12 21:19:19 +0200

    rosemary/device: Up clearkey HIDL to 1.4
    Signed-off-by: Woomymy <github@woomy.ovh>

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_PACKAGES += \
 
 # DRM, but bad
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     libclearkeycasplugin.vendor \
     libdrmclearkeyplugin.vendor \
     libmockdrmcryptoplugin
