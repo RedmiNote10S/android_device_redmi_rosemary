465f2f7c8ead7b3fa20f8a68c5eb936d3d0380d8
Beru Hinode <windowz414@1337.lgbt>
2022-09-15 20:39:09 +0000

    Revert "configs/media: Don't include mtk/google media codecs"
    This reverts commit 7ed7871f30e61a513b590fa4ac72c2efd4f1eb17.

--- device.mk
+++ device.mk
@@ -64,7 +64,7 @@ PRODUCT_COPY_FILES += \
 
 # Media codecs (from frameworks)
 PRODUCT_COPY_FILES += \
-    $(foreach xml, google_audio, \
+    $(foreach xml, google_audio google_video_le, \
         frameworks/av/media/libstagefright/data/media_codecs_$(xml).xml:$(addprefix $(TARGET_COPY_OUT_VENDOR)/etc/, media_codecs_$(xml).xml))
 
 # Audio
