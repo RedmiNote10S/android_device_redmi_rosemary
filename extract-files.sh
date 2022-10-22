6a967a159df89c4623b8f64685d5d02e06ef1bb0
SamarV-121 <samarvispute121@pm.me>
2022-10-22 15:38:54 +0000

    rosemary/extract-files: Shim DFPS libs with VNDK30 libutils
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,12 @@ function blob_fixup() {
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.6-impl.so )
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
+        vendor/lib/hw/dfps.mt6785.so )
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+        vendor/lib64/hw/dfps.mt6785.so )
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
     esac
 }
 
