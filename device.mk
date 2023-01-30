67153d6bcfb396ccc30f4c04d12a7a4ff5548fa3
Beru Hinode <windowz414@1337.lgbt>
2023-01-30 11:22:14 +0000

    Revert "rosemary: Import custom display configuration"
    This reverts commit 9dbe79aa632771a82ad98a71a434bc1bcd9fbd15.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- device.mk
+++ device.mk
@@ -166,11 +166,6 @@ PRODUCT_PACKAGES += \
     libhwc2onfbadapter \
     libfmq
 
-# Display Configuration
-PRODUCT_COPY_FILES += \
-    $(foreach file,$(wildcard $(DEVICE_PATH)/configs/display/*), \
-        $(file):$(addprefix $(TARGET_COPY_OUT_VENDOR)/etc/, $(notdir $(file))) )
-
 # MLIPay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.0.vendor \
